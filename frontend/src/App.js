7886342870ca0e2bc34982bf48c584e6e42b5eb9
Akhila <akhila@Sainarens-MacBook-Pro.local>
2022-05-25 17:50:57 -0700

    refined files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Nav, NavItem } from "react-bootstrap";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Purchased from "./components/Purchased/purchased";
+import Home from "./components/Home/home";
+import Wishlist from "./components/Wishlist/wishlist";
+import Search from "./components/Search/search";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <div className="navContainer">
+          <Nav className="ml-auto" style={{ backgroundColor: "black" }}>
+            <NavItem className="navs">
+              <Link className="nav-link" to="/home">
+                Home
+              </Link>
+            </NavItem>
+            <NavItem className="navs">
+              <Link className="nav-link" to="/search">
+                Search
+              </Link>
+            </NavItem>
+            <NavItem className="navs">
+              <Link className="nav-link" to="/wishlist">
+                Wishlist
+              </Link>
+            </NavItem>
+            <NavItem className="navs">
+              <Link className="nav-link" to="/purchased">
+                Purchased
+              </Link>
+            </NavItem>
+          </Nav>
+        </div>
+
+        <Routes>
+          <Route exact path="/home" element={<Home />}></Route>
+          <Route exact path="/wishlist" element={<Wishlist />}></Route>
+          <Route exact path="/search" element={<Search />}></Route>
+          <Route exact path="/purchased" element={<Purchased />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
