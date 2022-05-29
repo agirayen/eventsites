0a866cdb2581da03a9e680dde2bcff0a2bfb9124
siddhi mittal <mittal@pdx.edu>
2022-05-29 16:33:43 -0700

    setting up the server and dependencies

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,12 @@ const http =require("http");
 const fs =require ("fs");
 const path = require ("path");
 const server = http.createServer((req,res)=>{
-
+//request handling here
+res.writeHead(200,{"content-Type": "text/html"});
+res.write("Welcome to our services\n");
+res.end
 });
+// when we are going to use listen mentod then it use 9001 port number
 server.listen(9001,()=>{
     console.log("Server running on port 9001");
 });
