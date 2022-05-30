e43b6ff677eb8688018619b5e5a610f41c6e2cc4
siddhi mittal <mittal@pdx.edu>
2022-05-29 22:24:37 -0700

    created naive databse for the event users and checking for registerd users

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,35 @@
 const http =require("http");
 const fs =require ("fs");
 const path = require ("path");
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+ 
+app.use(cors());
+
+// change the whole work into express libraries
+const app = express();
+
+// here we are creating our own database to make further checking for the registered users
+let eventdatabase = {
+    users: [1,2,3,4,5],
+};
+
+// we are checking for the regiestered user otherwise we are sending errors
+app.get("/users/:userid", (req, res)=>{
+    let id= req.params.userid;
+    console.log(id);
+
+    const userFound = eventdatabase.users(id);
+    if (userFound) {
+        res.status(200).send("user is registered");
+    }else
+    {
+        res.status(404).send("user is not registered");
+    }
+
+});
+
 const server = http.createServer((req,res)=>{
 //request handling here
 res.writeHead(200,{"content-Type": "text/html"});
