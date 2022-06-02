577dff3b22c6c5865d71b262244565a8cd917773
siddhi mittal <mittal@pdx.edu>
2022-06-02 16:08:21 -0700

    server file updated and pool added

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,36 @@
 const http =require("http");
 const fs =require ("fs");
 const path = require ("path");
-import express from "express";
 import cors from "cors";
-import "dotenv/config";
- 
-app.use(cors());
-
-// change the whole work into express libraries
+const queryService = require("./pool");
+const bodyParser = require("body-parser");
+const express = require("express");
+const port = 3000;
 const app = express();
 
-// here we are creating our own database to make further checking for the registered users
-let eventdatabase = {
-    users: [1,2,3,4,5],
-};
+app.use(bodyParser.json());
+app.use(cors());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-// we are checking for the regiestered user otherwise we are sending errors
-app.get("/users/:userid", (req, res)=>{
-    let id= req.params.userid;
-    console.log(id);
+const { createUser, getUserByUsername } = queryService;
 
-    const userFound = eventdatabase.users(id);
-    if (userFound) {
-        res.status(200).send("user is registered");
-    }else
-    {
-        res.status(404).send("user is not registered");
-    }
-
+app.get("/", (req, res) => {
+  res.json({ responsetype: "service is up!!" });
 });
 
-const server = http.createServer((req,res)=>{
-//request handling here
-res.writeHead(200,{"content-Type": "text/html"});
-res.write("Welcome to our services\n");
-res.end
+app.post("/createUser", (req, res) => {
+  createUser(req, res);
+  // res.json({ responsetype: "service is up!!" });
 });
-// when we are going to use listen mentod then it use 9001 port number
-server.listen(9001,()=>{
-    console.log("Server running on port 9001");
+
+app.get("/getUser", (req, res) => {
+  getUserByUsername(req, res);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
