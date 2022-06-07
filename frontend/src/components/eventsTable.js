b744e65547b32c8f97a11fde8a95c4aae77ce0cc
Akhila <akhila@Sainarens-MacBook-Pro.local>
2022-06-07 02:05:40 -0700

    table and css updated

--- frontend/src/components/eventsTable.js
+++ frontend/src/components/eventsTable.js
@@ -1,37 +1,49 @@
 import React from "react";
-import { Row, Table, Container } from "react-bootstrap";
 
 const EventsTable = ({ data, handlePurchase, handleWhishlist }) => {
   return (
-    <Container>
-      <Row>
+    <div>
+      <table className="EventsTable">
+        <thead>
+          <tr>
+            <th>Event Name</th>
+            <th>City</th>
+            <th>Link </th>
+            <th>Address</th>
+            <th>Purchase</th>
+            <th>Wishlist</th>
+          </tr>
+        </thead>
         {data &&
           data.length &&
           data.map((event) => {
             return (
-              <>
-                <Row>
-                  <Table>
-                    <thead></thead>
-                    <tr>
-                      <td>{event.event_name}</td>
-                      <td>{event.name}</td>
-                      <td>{event.url}</td>
-                      <td>{event.address_line_1}</td>
-                      <td
-                        onClick={() => handlePurchase(event)}
-                      >{`Purchase`}</td>
-                      <td
-                        onClick={() => handleWhishlist(event)}
-                      >{`Wishlist`}</td>
-                    </tr>
-                  </Table>
-                </Row>
-              </>
+              <tbody>
+                <tr>
+                  <td>{event.event_name}</td>
+                  <td>{event.name}</td>
+                  <td>
+                    <a href={event.url} alt="link to event">
+                      Link to event
+                    </a>
+                  </td>
+                  <td>{event.address_line_1}</td>
+                  <td>
+                    <button onClick={() => handlePurchase(event)}>
+                      {`Purchase`}
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => handleWhishlist(event)}
+                    >{`Wishlist`}</button>
+                  </td>
+                </tr>
+              </tbody>
             );
           })}
-      </Row>
-    </Container>
+      </table>
+    </div>
   );
 };
 
